c45c16049e5ced3507c9f9f8e87e0231a94147cf
PeterR <solutionsgelo@gmail.com>
2019-08-06 15:39:34 +0200

    added prisma to updateUser mutation

--- src/resolvers/mutations/users.js
+++ src/resolvers/mutations/users.js
@@ -1,5 +1,3 @@
-import uuid from 'uuid/v4';
-
 const createUser = async (parent, { data }, { prisma }, info) => {
   const emailTaken = await prisma.exists.User({ email: data.email });
 
@@ -26,30 +24,22 @@ const deleteUser = async (parent, { id }, { prisma }, info) => {
   );
 };
 
-const updateUser = (parent, { id, data }, { db }, info) => {
-  const user = db.users.find(user => user.id === id);
+const updateUser = async (parent, { id, data }, { prisma }, info) => {
+  const userExists = await prisma.exists.User({ id });
 
-  if (!user) {
-    throw new Error('User not found.');
+  if (!userExists) {
+    throw new Error('User not found');
   }
 
-  if (typeof data.email === 'string') {
-    const emailTaken = db.users.some(user => user.email === data.email);
-    if (emailTaken) {
-      throw new Error('Email is already in use.');
-    }
-    user.email = data.email;
-  }
-
-  if (typeof data.name === 'string') {
-    user.name = data.name;
-  }
-
-  if (typeof data.age !== 'undefined') {
-    user.age = data.age;
-  }
-
-  return user;
+  return prisma.mutation.updateUser(
+    {
+      where: {
+        id
+      },
+      data
+    },
+    info
+  );
 };
 
 export { createUser, deleteUser, updateUser };
