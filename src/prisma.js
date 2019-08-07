10830fadd8f634a1becfb2bbcd6daaf21f098af9
PeterR <solutionsgelo@gmail.com>
2019-08-07 09:52:10 +0200

    added user password, locked down db

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,10 @@
 import { Prisma } from 'prisma-binding';
+import { endpoint, secret } from './config';
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://192.168.99.100:4466'
+  endpoint,
+  secret
 });
 
 export default prisma;
