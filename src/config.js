c7e9dcacaca36a1a9de96b36a5b99127293e7105
PeterR <solutionsgelo@gmail.com>
2019-08-09 14:46:28 +0200

    extracted prisma secret

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
 export const endpoint = 'http://192.168.99.100:4466';
-export const secret = 'prismasecretkaposztasteszta';
 export const jwtsecret = 'thisismyjwtsecretbtw';
--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ import { secret } from './config';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret,
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 });
 
